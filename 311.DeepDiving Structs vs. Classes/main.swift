68900b148db68d467d91956b9a8e37621e254d2a
latronixo <pamerra@mail.ru>
2025-03-28 10:59:47 +0300

    14:10 Демонстрация работы с массивом копий экземпляров структуры

--- 311.DeepDiving Structs vs. Classes/main.swift
+++ 311.DeepDiving Structs vs. Classes/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-var hero = ClassHero(name: "Iron Man", universe: "Marvel")
+var hero = StructHero(name: "Iron Man", universe: "Marvel")
 
 var anotherMarvelHero = hero
 anotherMarvelHero.name = "The Hulk"
