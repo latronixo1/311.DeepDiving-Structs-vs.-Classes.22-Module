2e9581066b57c5b0f8d2ef7c8acee474fb5a4ad5
latronixo <pamerra@mail.ru>
2025-03-28 10:49:53 +0300

    10:00 Демонстрация копирования экземпляра класса

--- 311.DeepDiving Structs vs. Classes/main.swift
+++ 311.DeepDiving Structs vs. Classes/main.swift
@@ -7,4 +7,10 @@
 
 import Foundation
 
+var hero = ClassHero(name: "Iron Man", universe: "Marvel")
 
+var anotherMarvelHero = hero
+anotherMarvelHero.name = "The Hulk"
+
+print("hero name = \(hero.name)")
+print("anotherMarvelHero name = \(anotherMarvelHero.name )")
